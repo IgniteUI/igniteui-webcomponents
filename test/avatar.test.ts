6daa6910be6fe5854cc9cfb43d78de773c0ddbd6
Silvia Ivanova <SIvanova@infragistics.com>
2021-07-14 16:46:22 +0300

    test(avatar): provide avatar component tests

---
+++ test/avatar.test.ts
@@ -0,0 +1,63 @@
+import { html, fixture, expect } from '@open-wc/testing';
+import { IgcAvatarComponent } from '../src/components/avatar/avatar';
+import '../igniteui-webcomponents.js';
+
+describe('Avatar', () => {
+  it('passes the a11y audit', async () => {
+    const el = await fixture<IgcAvatarComponent>(
+      html`<igc-avatar></igc-avatar>`
+    );
+
+    expect(el).shadowDom.to.be.accessible();
+  });
+
+  it('should initialize avatar component with default values', async () => {
+    const el = await fixture<IgcAvatarComponent>(
+      html`<igc-avatar></igc-avatar>`
+    );
+
+    expect(el.size).to.equal('small');
+    expect(el.shape).to.equal('square');
+  });
+
+  it('should set avatar size correctly', async () => {
+    const el = await fixture<IgcAvatarComponent>(
+      html`<igc-avatar size="small"></igc-avatar>`
+    );
+
+    expect(el.size).to.equal('small');
+    el.setAttribute('size', 'medium');
+    expect(el).dom.to.equal(`<igc-avatar size="medium"></igc-avatar>`);
+  });
+
+  it('should set avatar shape correctly', async () => {
+    const el = await fixture<IgcAvatarComponent>(
+      html`<igc-avatar shape="square"></igc-avatar>`
+    );
+
+    expect(el.shape).to.equal('square');
+
+    el.setAttribute('shape', 'rounded');
+    expect(el).dom.to.equal(`<igc-avatar shape="rounded"></igc-avatar>`);
+  });
+
+  it('should fallback to initials avatar when no image or wrong image source is provided', async () => {
+    const el = await fixture<IgcAvatarComponent>(
+      html`<igc-avatar initials="ab"></igc-avatar>`
+    );
+
+    expect(el.src).to.be.undefined;
+    expect(el).shadowDom.to.equal(
+      `<div part="native" role="img" aria-label="avatar" aria-roledescription="small square" class="square small">
+      <span part="initials">ab</span>
+      </div>`
+    );
+
+    el.setAttribute('src', 'abs');
+    expect(el).shadowDom.to.equal(
+      `<div part="native" role="img" aria-label="avatar" aria-roledescription="small square" class="square small">
+      <span part="initials">ab</span>
+      </div>`
+    );
+  });
+});
