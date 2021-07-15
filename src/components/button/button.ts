c0a59ec58d23cd04326956873598db8e7850c917
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2021-07-15 13:49:39 +0300

    docs(*): Explore generating documentation metadata

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -4,10 +4,21 @@ import { classMap } from 'lit/directives/class-map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { IgcButtonBaseComponent } from './button-base.js';
 
-// @customElement('igc-button')
+/**
+ * @element igc-button
+ *
+ * @slot prefix - Slot for projecting content at the start of the button.
+ * @slot suffix - Slot for projecting content at the end of the button.
+ *
+ * @cssprop --size - Duh!
+ *
+ * @csspart native - The native button/a element.
+ * @csspart prefix - The prefix container.
+ * @csspart suffix - The suffix container.
+ */
 export class IgcButtonComponent extends IgcButtonBaseComponent {
   @property()
-  type?: 'button' | 'reset' | 'submit';
+  type!: 'button' | 'reset' | 'submit';
 
   render() {
     return html`
