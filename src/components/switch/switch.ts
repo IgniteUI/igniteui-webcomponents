3d87e4c3c19d6a0fe389200c1bed6f70123be2c8
Silvia Ivanova <SIvanova@infragistics.com>
2021-07-26 11:17:38 +0300

    feat(switch): initial implementation

---
+++ src/components/switch/switch.ts
@@ -0,0 +1,68 @@
+import { html, LitElement } from 'lit';
+import { property, query } from 'lit/decorators.js';
+import { styles } from './switch.material.css';
+
+// @customElement('igc-switch')
+export class IgcSwitchComponent extends LitElement {
+  static styles = styles;
+
+  @query('input[type="radio"]')
+  input?: HTMLInputElement;
+
+  @property()
+  name?: string;
+
+  @property()
+  value?: string;
+
+  @property({ type: Boolean, reflect: true })
+  disabled = false;
+
+  @property({ type: Boolean, reflect: true })
+  checked = false;
+
+  @property({ type: Boolean, reflect: true })
+  required = false;
+
+  @property({ type: Boolean, reflect: true })
+  invalid = false;
+
+  @property({ reflect: true, attribute: 'label-position' })
+  labelPosition?: 'before' | 'after' = 'after';
+
+  handleClick() {
+    if (this.checked == true) {
+      this.checked = false;
+    } else {
+      this.checked = true;
+    }
+  }
+
+  handleMouseDown(event: MouseEvent) {
+    event.preventDefault();
+    this.input?.focus();
+  }
+
+  render() {
+    return html`
+      <label
+        part="base"
+        for="radio"
+        @mousedown="${this.handleMouseDown}">
+          <input
+            id="radio"
+            type="radio"
+            .disabled="${this.disabled}"
+            .checked="${this.checked}"
+            @click="${this.handleClick}"
+          />
+          <span part="control">
+            <span part="thumb">
+          </span>
+          <span part="label">
+            <slot></slot>
+          </span>
+      </label>
+    `;
+  }
+}
---
+++ stories/switch.stories.ts
@@ -0,0 +1,72 @@
+import { html } from 'lit-html';
+import '../igniteui-webcomponents.js';
+import { Story } from './story.js';
+import { ifDefined } from 'lit-html/directives/if-defined';
+
+export default {
+  title: 'Switch',
+  component: 'igc-switch',
+  argTypes: {
+    label: { control: 'text', defaultValue: 'Label' },
+    labelPosition: {
+      control: {
+        type: 'inline-radio',
+        options: ['before', 'after'],
+      },
+      defaultValue: 'after',
+    },
+    checked: {
+      control: 'boolean',
+      description: 'Determines whether the switch is checked.',
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+        defaultValue: {
+          summary: 'false',
+        },
+      },
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Determines whether the switch is disabled.',
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+        defaultValue: {
+          summary: 'false',
+        },
+      },
+    },
+  },
+};
+
+interface ArgTypes {
+  label: string;
+  labelPosition: 'before' | 'after';
+  checked: boolean;
+  disabled: boolean;
+}
+
+interface Context {
+  globals: { theme: string; direction: string };
+}
+
+const Template: Story<ArgTypes, Context> = (
+  { label, labelPosition, checked, disabled }: ArgTypes,
+  { globals: { direction } }: Context
+) => {
+  return html`
+    <igc-switch
+      label-position=${ifDefined(labelPosition)}
+      .checked=${ifDefined(checked)}
+      .disabled=${ifDefined(disabled)}
+      dir=${ifDefined(direction)}
+    >
+      ${label}
+    </igc-switch>
+  `;
+};
+
+export const Basic = Template.bind({});
