4312af1216096b3c8d9e79e7e38e85b10b18c18f
Simeon Simeonoff <sim.simeonoff@gmail.com>
2021-07-28 18:12:55 +0300

    refactor(stories): update radio-group story

--- stories/radio-group.stories.ts
+++ stories/radio-group.stories.ts
@@ -2,11 +2,15 @@ import { html } from 'lit-html';
 import '../igniteui-webcomponents.js';
 import { Story } from './story.js';
 import { ifDefined } from 'lit-html/directives/if-defined';
-import { IgcRadioComponent } from '../src/components/radio/radio.js';
 
 export default {
   title: 'Radio Group',
   component: 'igc-radio-group',
+  parameters: {
+    actions: {
+      handles: ['igcChange', 'igcFocus', 'igcBlur'],
+    },
+  },
   argTypes: {
     labelPosition: {
       control: {
@@ -51,37 +55,21 @@ const Template: Story<ArgTypes, Context> = (
   { alignment, labelPosition, disabled }: ArgTypes,
   { globals: { direction } }: Context
 ) => {
-  const handleChange = (e: Event) => {
-    console.log((e.target as IgcRadioComponent).value);
-  };
-
-  const handleFocus = (e: Event) => {
-    console.log('focused', e.target);
-  };
-
-  const handleBlur = (e: Event) => {
-    console.log('blurred', e.target);
-  };
-
   return html`
-  <div dir="${ifDefined(direction)}">
     <igc-radio-group
+      dir="${ifDefined(direction)}"
       alignment="${ifDefined(alignment)}"
       label-position="${ifDefined(labelPosition)}"
       .disabled="${ifDefined(disabled)}"
-      @igcChange="${handleChange}"
     >
       <igc-radio
         name="fruit"
         value="apple"
-        @igcFocus="${handleFocus}"
-        @igcBlur="${handleBlur}"
       >Apple</igc-radio>
       <igc-radio name="fruit" value="orange" checked>Orange</igc-radio>
       <igc-radio name="fruit" value="mango">Mango</igc-radio>
       <igc-radio name="fruit" value="banana" disabled>Banana</igc-radio>
     </igc-radio>
-  </div>
 `;
 };
 
