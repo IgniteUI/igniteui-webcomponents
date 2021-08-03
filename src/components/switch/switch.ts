8c5e5e5fce69d827923d137db21860434546bd3c
Silvia Ivanova <SIvanova@infragistics.com>
2021-08-03 16:05:04 +0100

    feat(switch): events, tests, theme improvements

--- src/components/switch/switch.ts
+++ src/components/switch/switch.ts
@@ -1,19 +1,40 @@
 import { html, LitElement } from 'lit';
-import { property, query } from 'lit/decorators.js';
+import { property, query, state } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { live } from 'lit/directives/live.js';
+import { EventEmitterMixin } from '../common/mixins/event-emitter.js';
+import { Constructor } from '../common/mixins/constructor.js';
 import { styles } from './switch.material.css';
 
+let nextId = 0;
+
+export interface IgcSwitchEventMap {
+  igcChange: CustomEvent<void>;
+  igcFocus: CustomEvent<void>;
+  igcBlur: CustomEvent<void>;
+}
+
 // @customElement('igc-switch')
-export class IgcSwitchComponent extends LitElement {
+export class IgcSwitchComponent extends EventEmitterMixin<
+  IgcSwitchEventMap,
+  Constructor<LitElement>
+>(LitElement) {
   static styles = styles;
 
-  @query('input[type="radio"]')
-  input?: HTMLInputElement;
+  private inputId = `switch-${nextId++}`;
+  private labelId = `switch-label-${this.inputId}`;
+
+  @query('input[type="checkbox"]')
+  input!: HTMLInputElement;
+
+  @state()
+  _tabIndex = 0;
 
   @property()
-  name?: string;
+  name!: string;
 
   @property()
-  value?: string;
+  value!: string;
 
   @property({ type: Boolean, reflect: true })
   disabled = false;
@@ -28,35 +49,85 @@ export class IgcSwitchComponent extends LitElement {
   invalid = false;
 
   @property({ reflect: true, attribute: 'label-position' })
-  labelPosition?: 'before' | 'after' = 'after';
+  labelPosition: 'before' | 'after' = 'after';
+
+  @property({ reflect: true, attribute: 'aria-labelledby' })
+  ariaLabelledby!: string;
+
+  click() {
+    this.input.click();
+  }
+
+  focus(options?: FocusOptions) {
+    this.input.focus(options);
+  }
+
+  blur() {
+    this.input.blur();
+  }
+
+  reportValidity() {
+    return this.input.reportValidity();
+  }
+
+  setCustomValidity(message: string) {
+    this.input.setCustomValidity(message);
+    this.invalid = !this.input.checkValidity();
+  }
 
   handleClick() {
-    if (this.checked == true) {
-      this.checked = false;
-    } else {
-      this.checked = true;
-    }
+    this.checked = !this.checked;
+  }
+
+  handleBlur() {
+    this.emitEvent('igcBlur');
+  }
+
+  handleFocus() {
+    this.emitEvent('igcFocus');
   }
 
   handleMouseDown(event: MouseEvent) {
     event.preventDefault();
-    this.input?.focus();
+    this.input.focus();
+  }
+
+  handleChange() {
+    if (this.checked) {
+      this.input.focus();
+      this._tabIndex = 0;
+      this.emitEvent('igcChange');
+    }
   }
 
   render() {
     return html`
-      <label part="base" for="radio" @mousedown="${this.handleMouseDown}">
+      <label
+        part="base"
+        for="${this.inputId}"
+        @mousedown="${this.handleMouseDown}"
+      >
         <input
-          id="radio"
-          type="radio"
+          id=${this.inputId}
+          type="checkbox"
+          name="${ifDefined(this.name)}"
+          value="${ifDefined(this.value)}"
           .disabled="${this.disabled}"
-          .checked="${this.checked}"
+          .checked="${live(this.checked)}"
+          tabindex=${this._tabIndex}
+          aria-checked="${this.checked ? 'true' : 'false'}"
+          aria-disabled="${this.disabled ? 'true' : 'false'}"
+          aria-labelledby="${this.ariaLabelledby
+            ? this.ariaLabelledby
+            : this.labelId}"
           @click="${this.handleClick}"
+          @blur="${this.handleBlur}"
+          @focus="${this.handleFocus}"
         />
         <span part="control">
           <span part="thumb"></span>
         </span>
-        <span part="label">
+        <span part="label" id="${this.labelId}">
           <slot></slot>
         </span>
       </label>
