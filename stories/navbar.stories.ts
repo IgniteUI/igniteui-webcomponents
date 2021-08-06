cd28fe7415f2f49c42589c2929e505dd6df28c93
Maria Tsvyatkova <mtsvyatkova@infragistics.com>
2021-08-06 09:39:42 +0300

    feat(navbar): update story #9

--- stories/navbar.stories.ts
+++ stories/navbar.stories.ts
@@ -1,23 +1,41 @@
 import { html } from 'lit-html';
+import { ifDefined } from 'lit-html/directives/if-defined';
 import '../igniteui-webcomponents.js';
 import { Story } from './story';
 
 export default {
   title: 'Navigation Bar',
   component: 'igc-navbar',
+  argTypes: {
+    content: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Title',
+    },
+  },
 };
+
+interface ArgTypes {
+  content: string;
+}
 
 interface Context {
   globals: { theme: string; direction: 'ltr' | 'rtl' | 'auto' };
 }
 
-const Template: Story<{}, Context> = () => {
+const Template: Story<ArgTypes, Context> = (
+  { content = 'Title' }: ArgTypes,
+  { globals: { direction } }: Context
+) => {
   return html`
-    <igc-navbar>
-      <button slot="start">Home</button>
-      <h1>This is the title</h1>
-      <button slot="end">More</button>
-    </igc-navbar>
+    <div>
+      <igc-navbar dir=${ifDefined(direction)}>
+        <button slot="start">IG</button>
+        <h1>${content}</h1>
+        <button slot="end">More</button>
+      </igc-navbar>
+    </div>
   `;
 };
 
