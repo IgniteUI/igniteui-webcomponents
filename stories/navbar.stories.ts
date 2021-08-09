5a70c4a526f8464715263544d3a5b6bba7e73adc
Maria Tsvyatkova <mtsvyatkova@infragistics.com>
2021-08-09 09:59:00 +0300

    chore(*): add icons to the story #9

--- stories/navbar.stories.ts
+++ stories/navbar.stories.ts
@@ -1,6 +1,7 @@
 import { html } from 'lit-html';
 import { ifDefined } from 'lit-html/directives/if-defined';
 import '../igniteui-webcomponents.js';
+import { registerIcon } from '../src/components/icon/icon.registry.js';
 import { Story } from './story';
 
 export default {
@@ -24,18 +25,26 @@ interface Context {
   globals: { theme: string; direction: 'ltr' | 'rtl' | 'auto' };
 }
 
+registerIcon(
+  'home',
+  'https://unpkg.com/material-design-icons@3.0.1/action/svg/production/ic_home_24px.svg'
+);
+
+registerIcon(
+  'search',
+  'https://unpkg.com/material-design-icons@3.0.1/action/svg/production/ic_search_24px.svg'
+);
+
 const Template: Story<ArgTypes, Context> = (
   { content = 'Title' }: ArgTypes,
   { globals: { direction } }: Context
 ) => {
   return html`
-    <div>
-      <igc-navbar dir=${ifDefined(direction)}>
-        <button slot="start">IG</button>
-        <h1>${content}</h1>
-        <button slot="end">More</button>
-      </igc-navbar>
-    </div>
+    <igc-navbar dir=${ifDefined(direction)} style="height:30px">
+      <igc-icon slot="start" name="home"></igc-icon>
+      <h2>${content}</h2>
+      <igc-icon slot="end" name="search"></igc-icon>
+    </igc-navbar>
   `;
 };
 
