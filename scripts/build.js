1ebccbf76aa86fcac9b72090ffb69d7c0d5241a4
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2021-10-07 10:54:47 +0300

    build: Added publish actions
    Added CHANGELOG.md
    Updated @lit to latest official version

--- scripts/build.js
+++ scripts/build.js
@@ -6,28 +6,37 @@ const exec = promisify(require('child_process').exec);
 const copyFile = promisify(fs.copyFile);
 
 
-const DEST_DIR = path.resolve(__dirname, '../dist');
+const DEST_DIR = path.join.bind(null, path.resolve(__dirname, '../dist'));
 
 
 (async () => {
   await exec('npm run clean');
 
-  console.log('Inlining styles...');
+  console.info('Inlining styles...');
   await exec('npm run build:styles');
 
-  console.log(`Transpiling...`);
+  console.info(`Building...`);
   await exec(`tsc -p tsconfig.prod.json`);
 
-  console.log(`Generating custom-elements.json file...`);
+  console.info(`Generating custom-elements.json file...`);
   await exec(`npm run build:docs:schema`);
-  await copyFile('custom-elements.json', path.join(DEST_DIR, 'custom-elements.json'));
+  await copyFile('custom-elements.json', DEST_DIR('custom-elements.json'));
 
-  console.log(`Generating VSCode custom data file...`);
+  console.info(`Generating VSCode custom data file...`);
   await exec(`npm run build:docs:vscode-schema`);
-  await copyFile('vscode-html-custom-data.json', path.join(DEST_DIR, 'vscode-html-custom-data.json'));
+  await copyFile('vscode-html-custom-data.json', DEST_DIR('vscode-html-custom-data.json'));
 
-  console.log(`Copying package.json and README...`);
-  await copyFile('scripts/_package.json', path.join(DEST_DIR, 'package.json'));
+  console.info(`Generating package.json...`);
+  await copyFile('scripts/_package.json', DEST_DIR('package.json'));
+
+  console.info(`Copying CHANGELOG.md...`);
+  await copyFile('CHANGELOG.md', DEST_DIR('CHANGELOG.md'));
+
+  console.info(`Copying LICENSE...`);
+  await copyFile('LICENSE', DEST_DIR('LICENSE'));
+
+  console.info(`Copying README.md...`);
+  await copyFile('README.md', DEST_DIR('README.md'));
 
   console.log(`Done! 🎉`);
  })();
