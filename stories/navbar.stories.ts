ea2f45b0c24af1706cf4a054bd9b4caa387ad9b0
MPopov <mspopovv@gmail.com>
2021-12-01 13:33:41 +0200

    fix: (navbar) outline button cut off. also make sure that our atomic components work well with the navbar

--- stories/navbar.stories.ts
+++ stories/navbar.stories.ts
@@ -19,8 +19,8 @@ registerIcon(
 );
 
 registerIcon(
-  'search',
-  'https://unpkg.com/material-design-icons@3.0.1/action/svg/production/ic_search_24px.svg'
+  'favorite',
+  'https://unpkg.com/material-design-icons@3.0.1/action/svg/production/ic_favorite_24px.svg'
 );
 
 const Template: Story<any, Context> = (
@@ -31,7 +31,25 @@ const Template: Story<any, Context> = (
     <igc-navbar dir=${ifDefined(direction)} style="height:30px">
       <igc-icon slot="start" name="home"></igc-icon>
       <h2>${content}</h2>
-      <igc-icon slot="end" name="search"></igc-icon>
+      <igc-input
+        slot="end"
+        style="align-self: center"
+        type="search"
+        placeholder="search"
+        size="small"
+        outlined
+      >
+        <igc-icon name="search" slot="suffix"></igc-icon>
+      </igc-input>
+      <igc-icon slot="end" name="favorite"></igc-icon>
+      <igc-avatar
+        slot="end"
+        size="small"
+        shape="circle"
+        src="https://i.pravatar.cc/200"
+        >MP</igc-avatar
+      >
+      <igc-button slot="end" name="search">Login</igc-button>
     </igc-navbar>
   `;
 };
