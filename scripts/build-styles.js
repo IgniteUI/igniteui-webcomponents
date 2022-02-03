b153194a3e09c6255bcca3bbc2db3c7c16b08812
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-02-03 11:49:15 +0200

    build(styles): generate css for all theme variants

--- scripts/build-styles.js
+++ scripts/build-styles.js
@@ -39,7 +39,11 @@ async function sassRender(sourceFile, templateFile, outputFile) {
 
 (async () => {
   const template = path.resolve(process.argv[1], '../styles.tmpl');
-  const paths = await globby(['src/components/**/*.material.scss']);
+  const paths = await globby([
+    'src/components/**/*.material.scss',
+    'src/components/**/*.bootstrap.scss',
+    'src/components/**/*.indigo.scss', 'src/components/**/*.fluent.scss'
+  ]);
 
   for (const sourceFile of paths) {
     const output = sourceFile.replace(/\.scss$/, '.css.ts');
