cfa72b9b49ebd1a8e3aaa675424f729d2b8f0c30
Dilyana Dimova <didimmova@Dilyanas-MacBook-Pro.local>
2022-02-03 11:09:18 +0200

    feat(toast): add tests for the methods

--- src/components/toast/toast.spec.ts
+++ src/components/toast/toast.spec.ts
@@ -12,46 +12,82 @@ describe('Toast', () => {
     defineComponents(IgcToastComponent);
   });
 
-  let el: IgcToastComponent;
+  let toast: IgcToastComponent;
 
   describe('', () => {
     beforeEach(async () => {
-      el = await createToastComponent();
+      toast = await createToastComponent();
     });
 
     it('passes the a11y audit', async () => {
-      const el = await fixture<IgcToastComponent>(
+      const toast = await fixture<IgcToastComponent>(
         html`<igc-toast></igc-toast>`
       );
 
-      await expect(el).shadowDom.to.be.accessible();
+      await expect(toast).shadowDom.to.be.accessible();
     });
 
     it('should render content inside the toast', async () => {
       const message = 'Toast message';
-      const el = await fixture<IgcToastComponent>(
+      const toast = await fixture<IgcToastComponent>(
         html`<igc-toast>${message}</igc-toast>`
       );
-      expect(el).dom.to.have.text(message);
+      expect(toast).dom.to.have.text(message);
     });
 
     it('should change the toast display time correctly', async () => {
-      el.setAttribute('display-time', `10000`);
-      await elementUpdated(el);
-      expect(el.displayTime).to.eq(10000);
-      expect(el).dom.to.equal(`<igc-toast display-time='10000'></igc-toast>`);
+      toast.setAttribute('display-time', `10000`);
+      await elementUpdated(toast);
+      expect(toast.displayTime).to.eq(10000);
+      expect(toast).dom.to.equal(
+        `<igc-toast display-time='10000'></igc-toast>`
+      );
     });
 
     it('should change the toast keepOpen option correctly.', async () => {
-      el.keepOpen = true;
-      expect(el.keepOpen).to.be.true;
-      await elementUpdated(el);
-      expect(el).dom.to.equal(`<igc-toast keep-open></igc-toast>`);
+      toast.keepOpen = true;
+      expect(toast.keepOpen).to.be.true;
+      await elementUpdated(toast);
+      expect(toast).dom.to.equal(`<igc-toast keep-open></igc-toast>`);
 
-      el.keepOpen = false;
-      expect(el.keepOpen).to.be.false;
-      await elementUpdated(el);
-      expect(el).dom.to.equal(`<igc-toast></igc-toast>`);
+      toast.keepOpen = false;
+      expect(toast.keepOpen).to.be.false;
+      await elementUpdated(toast);
+      expect(toast).dom.to.equal(`<igc-toast></igc-toast>`);
+    });
+
+    it('show method should open the toast', async () => {
+      toast.open = false;
+      await elementUpdated(toast);
+
+      toast.show();
+      await elementUpdated(toast);
+      expect(toast.open).to.eq(true);
+    });
+
+    it('hide method should close the toast', async () => {
+      toast.open = true;
+      await elementUpdated(toast);
+
+      toast.hide();
+      await elementUpdated(toast);
+      expect(toast.open).to.eq(false);
+    });
+
+    it('toggle method should toggle the toast', async () => {
+      toast.open = true;
+      await elementUpdated(toast);
+
+      toast.toggle();
+      await elementUpdated(toast);
+      expect(toast.open).to.eq(false);
+
+      toast.open = false;
+      await elementUpdated(toast);
+
+      toast.toggle();
+      await elementUpdated(toast);
+      expect(toast.open).to.eq(true);
     });
 
     const createToastComponent = (template = `<igc-toast></igc-toast>`) => {
