ac323db6d07b50e42e2a5a1e16ce89ad8ea3b659
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-03-11 14:48:31 +0200

    refactor(chip): add dir to stories

--- stories/chip.stories.ts
+++ stories/chip.stories.ts
@@ -73,20 +73,24 @@ registerIconFromText(
   '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></svg>'
 );
 
-const ChipTemplate: Story<ArgTypes, Context> = ({
-  disabled,
-  removable,
-  selectable,
-  selected,
-  size = 'medium',
-  variant,
-}: ArgTypes) => html`
+const ChipTemplate: Story<ArgTypes, Context> = (
+  {
+    disabled,
+    removable,
+    selectable,
+    selected,
+    size = 'medium',
+    variant,
+  }: ArgTypes,
+  { globals: { direction } }: Context
+) => html`
   <igc-chip
     .disabled="${disabled}"
     .removable=${removable}
     .selectable=${selectable}
     .selected=${selected}
     .size=${size}
+    dir=${direction}
     variant=${ifDefined(variant)}
   >
     Chip
