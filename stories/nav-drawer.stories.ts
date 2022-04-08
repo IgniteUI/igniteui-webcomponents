4106b7a6f4243cbe8438df546b02d3ab6f2014b9
Dilyana Dimova <didimmova@Dilyanas-MacBook-Pro.local>
2022-04-08 09:43:08 +0300

    fix(navdrawer): remove fixed height

--- stories/nav-drawer.stories.ts
+++ stories/nav-drawer.stories.ts
@@ -178,7 +178,7 @@ const Template: Story<ArgTypes, Context> = (
   return html`
     <div
       class="igc-scrollbar"
-      style="display: flex; margin-top: -8px; margin-left: -8px;"
+      style="display: flex; margin-top: -8px; margin-left: -8px; height: 100vh;"
     >
       <igc-nav-drawer
         dir=${ifDefined(direction)}
