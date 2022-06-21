18e82e20947125ddc1065e3564fc17970ee24b21
ddaribo <bozhidara.pp@gmail.com>
2022-06-21 12:44:08 +0300

    feat(accordion): basic tests

--- src/components/accordion/accordion.spec.ts
+++ src/components/accordion/accordion.spec.ts
@@ -1,15 +1,22 @@
-import { html, fixture, expect, unsafeStatic } from '@open-wc/testing';
-import { defineComponents } from '../../index';
-import IgcAccordionComponent from './accordion';
+import {
+  html,
+  fixture,
+  expect,
+  unsafeStatic,
+  elementUpdated,
+} from '@open-wc/testing';
+import { defineComponents } from '../../index.js';
+import IgcAccordionComponent from './accordion.js';
+import IgcExpansionPanelComponent from '../expansion-panel/expansion-panel.js';
 
 describe('Accordion', () => {
   before(() => {
-    defineComponents(IgcAccordionComponent);
+    defineComponents(IgcExpansionPanelComponent, IgcAccordionComponent);
   });
 
   let accordion: IgcAccordionComponent;
 
-  describe('', () => {
+  describe('Basic', () => {
     beforeEach(async () => {
       accordion = await createAccordionComponent(testTemplate);
     });
@@ -18,9 +25,25 @@ describe('Accordion', () => {
       await expect(accordion).shadowDom.to.be.accessible();
     });
 
-    it('Should render accordion w/ expansion panels', async () => {
+    it('Should render accordion w/ expansion panels and calculate them properly', async () => {
       expect(accordion.panels.length).to.equal(3);
       expect(accordion).to.contain('igc-expansion-panel');
+    });
+
+    it('Should be able to render nested accordions', async () => {
+      accordion = await createAccordionComponent(nestedAccTemplate);
+      expect(accordion.panels.length).to.equal(1);
+
+      accordion.panels[0].show();
+      await elementUpdated(accordion);
+
+      const contentSlot = accordion.panels[0].shadowRoot!.querySelector(
+        'slot:not([name])'
+      ) as HTMLSlotElement;
+      const contentElements = contentSlot.assignedElements();
+      expect(contentElements[0].tagName.toLowerCase()).to.equal(
+        'igc-accordion'
+      );
     });
 
     const createAccordionComponent = (
@@ -53,6 +76,26 @@ const testTemplate = `
         </h1>
         <h2 slot="subtitle">Expansion panel 3 subtitle</h2>
         <p>Sample content 3</p> 
+    </igc-expansion-panel>
+</igc-accordion>
+`;
+
+const nestedAccTemplate = `
+<igc-accordion>
+    <igc-expansion-panel>
+        <h1 slot="title">
+            Expansion panel 1 title
+        </h1>
+        <h2 slot="subtitle">Expansion panel 1 subtitle</h2>
+        <igc-accordion>
+          <igc-expansion-panel>
+              <h1 slot="title">
+                  Expansion panel 1.1 title
+              </h1>
+              <h2 slot="subtitle">Expansion panel 1.1 subtitle</h2>
+              <p>Sample content 1.1</p> 
+          </igc-expansion-panel>
+        </igc-accordion>
     </igc-expansion-panel>
 </igc-accordion>
 `;
