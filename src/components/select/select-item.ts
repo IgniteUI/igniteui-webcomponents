9981a4a7886bf0c57368516d42ebebb47744b82e
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-06-24 16:33:43 +0300

    refactor(select-item): implement textContent

--- src/components/select/select-item.ts
+++ src/components/select/select-item.ts
@@ -1,11 +1,32 @@
+import { queryAssignedNodes } from 'lit/decorators.js';
 import IgcDropdownItemComponent from '../dropdown/dropdown-item';
 
 export default class IgcSelectItemComponent extends IgcDropdownItemComponent {
+  @queryAssignedNodes({ flatten: true })
+  private content!: Array<Element>;
+
   public static override readonly tagName =
     'igc-select-item' as 'igc-dropdown-item';
 
   constructor() {
     super();
+  }
+
+  protected override firstUpdated() {
+    console.log(this.content);
+  }
+
+  public override get textContent() {
+    return this.content
+      .map((t) => t.textContent?.trim())
+      .filter((t) => t !== '')
+      .join(' ');
+  }
+
+  public override set textContent(value: string) {
+    const text = new Text(value);
+    this.content.forEach((n) => n.remove());
+    this.appendChild(text);
   }
 }
 
