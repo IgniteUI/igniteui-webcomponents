25a701c707c0f45e12beec9a43a0afe6117b6056
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-07-14 10:09:31 +0300

    refactor(rating): remove unused types and fix disabled colors

--- src/components/common/types.ts
+++ src/components/common/types.ts
@@ -1,10 +1,1 @@
 export type Direction = 'ltr' | 'rtl' | 'auto';
-
-export interface ElementClipOptions {
-  width: number;
-  clip: number;
-  direction?: 'forward' | 'backward';
-  rtl?: boolean;
-  exact?: boolean;
-  matcher?: number;
-}
