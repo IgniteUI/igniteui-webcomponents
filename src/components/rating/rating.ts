0c7af6da3956b902021746fc1c7e253b4845073a
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-07-15 14:17:34 +0300

    fix(rating): step and line-height

--- src/components/rating/rating.ts
+++ src/components/rating/rating.ts
@@ -157,7 +157,10 @@ export default class IgcRatingComponent extends SizableMixin(
 
   @watch('single')
   protected handleSelectionChange() {
-    this.step = 1;
+    if (this.single) {
+      this.step = 1;
+      this.value = Math.ceil(this.value);
+    }
   }
 
   constructor() {
