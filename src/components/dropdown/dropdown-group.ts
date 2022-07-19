e30d1e3cf9fbff4daf71b1d35595066dc36ac70f
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2022-07-19 11:41:31 +0300

    refactor(dropdown-group): Simplified types

--- src/components/dropdown/dropdown-group.ts
+++ src/components/dropdown/dropdown-group.ts
@@ -4,23 +4,8 @@ import { themes } from '../../theming/theming-decorator.js';
 import { styles } from './themes/light/dropdown-group.base.css.js';
 import { styles as fluent } from './themes/light/dropdown-group.fluent.css.js';
 import type IgcDropdownItemComponent from './dropdown-item';
-import type IgcDropdownComponent from './dropdown';
 import { blazorSuppress } from '../common/decorators/blazorSuppress.js';
-
-export abstract class BaseDropdownGroupComponent<Parent> extends LitElement {
-  /** The parent dropdown/element that owns this component */
-  protected abstract parent: Parent;
-
-  /** Implement this to return the parent element owning this component */
-  protected abstract getParent(): Parent;
-
-  public override connectedCallback() {
-    super.connectedCallback();
-
-    this.setAttribute('role', 'group');
-    this.parent = this.getParent();
-  }
-}
+import { SizableInterface } from '../common/mixins/sizable.js';
 
 /**
  * @element igc-dropdown-group - A container for a group of `igc-dropdown-item` components.
@@ -31,16 +16,23 @@ export abstract class BaseDropdownGroupComponent<Parent> extends LitElement {
  * @csspart label - The native label element.
  */
 @themes({ fluent })
-export default class IgcDropdownGroupComponent extends BaseDropdownGroupComponent<IgcDropdownComponent> {
+export default class IgcDropdownGroupComponent extends LitElement {
   public static readonly tagName = 'igc-dropdown-group';
 
   public static override styles = styles;
-  protected parent!: IgcDropdownComponent;
+  protected parent!: SizableInterface;
 
   /** All child `igc-dropdown-item`s. */
   @blazorSuppress()
   @queryAssignedElements({ flatten: true, selector: 'igc-dropdown-item' })
   public items!: Array<IgcDropdownItemComponent>;
+
+  public override connectedCallback() {
+    super.connectedCallback();
+
+    this.setAttribute('role', 'group');
+    this.parent = this.getParent();
+  }
 
   protected getParent() {
     return this.closest('igc-dropdown')!;
