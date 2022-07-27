13ff1c799b97ff13cf9181dc304e31f492a99ee0
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-07-27 16:11:01 +0300

    build(styles): refactor build and watcher

--- scripts/build-styles.mjs
+++ scripts/build-styles.mjs
@@ -1,40 +1,9 @@
-import autoprefixer from 'autoprefixer';
-import { readFile, writeFile } from 'fs/promises';
 import { globby } from 'globby';
-import path from 'path';
-import postcss from 'postcss';
-import sass from 'sass';
 import report from './report.js';
-
-const renderSass = sass.compile;
-
-async function sassToCss(sassFile) {
-  const result = renderSass(sassFile, {
-    outputStyle: 'compressed'
-  });
-  let cssStr = result.css.toString();
-
-  cssStr = await postcss([autoprefixer]).process(cssStr).css;
-
-  // Strip BOM if any
-  if (cssStr.charCodeAt(0) === 0xfeff) {
-    cssStr = cssStr.substring(1);
-  }
-  return cssStr;
-}
-
-async function sassRender(sourceFile, templateFile, outputFile) {
-  const regex = /<%\s*content\s*%>/;
-  const template = await readFile(templateFile, 'utf-8');
-  const replacement = await sassToCss(sourceFile);
-  const newContent = template.replace(regex, replacement);
-  console.info('Compiling styles', sourceFile);
-
-  return writeFile(outputFile, newContent, 'utf-8');
-}
+import { sassRender, template } from './sass.mjs';
 
 (async () => {
-  const template = path.resolve(process.argv[1], '../styles.tmpl');
+  const startTime = new Date();
   const paths = await globby([
     'src/components/**/*.base.scss',
     'src/components/**/*.material.scss',
@@ -50,5 +19,5 @@ async function sassRender(sourceFile, templateFile, outputFile) {
       process.exit(-1);
     });
   }
-  report.success('Styles generation finished');
+  report.success(`Styles generated in ${Math.round((Date.now() - startTime) / 1000)}s`);
 })();
---
+++ scripts/styles-watcher.mjs
@@ -0,0 +1,34 @@
+import watch from 'node-watch';
+import report from './report.js';
+import { sassRender, template } from './sass.mjs';
+
+const watchOptions = {
+  recursive: true,
+  filter: (path) => {
+    return /.(?:scss)$/.test(path);
+  },
+};
+
+watch(['src'], watchOptions, function(_event, fileName) {
+  addToQueue(fileName);
+});
+
+let updating = false;
+
+async function addToQueue(fileName) {
+  const output = fileName.replace(/\.scss$/, '.css.ts');
+  if (updating) {
+    return;
+  }
+  console.log(`Change detected: ${fileName}`);
+  updating = true;
+  console.log('Rebuilding styles...');
+
+  await sassRender(fileName, template, output).catch((err) => {
+    report.error(err);
+  });
+  console.log('Styles build completed.');
+  updating = false;
+}
+
+console.log('Styles watcher started...');
