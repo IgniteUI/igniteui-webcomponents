47758f798314d89a6fd57fbe3cce3205230a1791
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2022-08-16 13:30:40 +0300

    refactor: Reduced LTR query calls and improved rating rendering
    - Extracted `isLTR` to an external helper function.
    - Reduced number of LTR query calls in certain scenarios to avoid
      reflow thrashing.
    - Cached rating symbols rendering logic. Properties not related
      to the symbols state will not trigger a DOM re-render.

--- src/components/rating/rating.ts
+++ src/components/rating/rating.ts
@@ -1,19 +1,14 @@
 import { html, LitElement, nothing } from 'lit';
-import {
-  property,
-  query,
-  queryAssignedElements,
-  queryAssignedNodes,
-  state,
-} from 'lit/decorators.js';
+import { property, query, queryAssignedNodes, state } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { styleMap } from 'lit/directives/style-map.js';
+import { guard } from 'lit/directives/guard.js';
 import { themes } from '../../theming/theming-decorator.js';
 import { watch } from '../common/decorators/watch.js';
 import { Constructor } from '../common/mixins/constructor.js';
 import { EventEmitterMixin } from '../common/mixins/event-emitter.js';
 import { SizableMixin } from '../common/mixins/sizable.js';
-import { clamp } from '../common/util.js';
+import { clamp, isLTR } from '../common/util.js';
 import { styles } from './rating.base.css.js';
 import { styles as bootstrap } from './rating.bootstrap.css.js';
 import { styles as fluent } from './rating.fluent.css.js';
@@ -61,14 +56,13 @@ export default class IgcRatingComponent extends SizableMixin(
 
   public static styles = [styles];
 
+  protected ratingSymbols: Array<IgcRatingSymbolComponent> = [];
+
   @query('[part="symbols"]', true)
   protected container!: HTMLElement;
 
   @queryAssignedNodes({ slot: 'value-label', flatten: true })
   protected valueLabel!: Array<Node>;
-
-  @queryAssignedElements({ selector: 'igc-rating-symbol' })
-  protected ratingSymbols!: Array<IgcRatingSymbolComponent>;
 
   @state()
   protected hoverValue = -1;
@@ -82,12 +76,6 @@ export default class IgcRatingComponent extends SizableMixin(
 
   protected get hasProjectedSymbols() {
     return this.ratingSymbols.length > 0;
-  }
-
-  protected get isLTR() {
-    return (
-      window.getComputedStyle(this).getPropertyValue('direction') === 'ltr'
-    );
   }
 
   protected get valueText() {
@@ -215,15 +203,16 @@ export default class IgcRatingComponent extends SizableMixin(
     }
 
     let result = this.value;
+    const ltr = isLTR(this);
 
     switch (key) {
       case 'ArrowUp':
       case 'ArrowRight':
-        result += this.isLTR ? this.step : -this.step;
+        result += ltr ? this.step : -this.step;
         break;
       case 'ArrowDown':
       case 'ArrowLeft':
-        result -= this.isLTR ? this.step : -this.step;
+        result -= ltr ? this.step : -this.step;
         break;
       case 'Home':
         result = this.step;
@@ -243,16 +232,25 @@ export default class IgcRatingComponent extends SizableMixin(
     }
   }
 
-  protected handleSlotChange() {
+  protected handleSlotChange(event: Event) {
+    const slot = event.target as HTMLSlotElement;
+
+    this.ratingSymbols = slot
+      .assignedElements()
+      .filter(
+        (el) => el instanceof IgcRatingSymbolComponent
+      ) as IgcRatingSymbolComponent[];
+
     if (this.hasProjectedSymbols) {
       this.max = this.ratingSymbols.length;
     }
+
     this.requestUpdate();
   }
 
   protected calcNewValue(x: number) {
     const { width, left, right } = this.container.getBoundingClientRect();
-    const percent = this.isLTR ? (x - left) / width : (right - x) / width;
+    const percent = isLTR(this) ? (x - left) / width : (right - x) / width;
     const value = this.round(this.max * percent + this.step / 2);
 
     return clamp(value, this.step, this.max);
@@ -274,20 +272,21 @@ export default class IgcRatingComponent extends SizableMixin(
     const exclusive = progress === 0 || this.value === index + 1 ? 0 : 1;
     const selection = this.single ? exclusive : progress;
     const activate = (p: number) => clamp(p * 100, 0, 100);
+    const ltr = isLTR(this);
 
     const forward = `inset(0 ${activate(
-      this.isLTR ? selection : 1 - selection
+      ltr ? selection : 1 - selection
     )}% 0 0)`;
     const backward = `inset(0 0 0 ${activate(
-      this.isLTR ? 1 - selection : selection
+      ltr ? 1 - selection : selection
     )}%)`;
 
     switch (direction) {
       case 'backward':
-        return this.isLTR ? backward : forward;
+        return ltr ? backward : forward;
       case 'forward':
       default:
-        return this.isLTR ? forward : backward;
+        return ltr ? forward : backward;
     }
   }
 
@@ -348,7 +347,13 @@ export default class IgcRatingComponent extends SizableMixin(
   }
 
   protected override render() {
-    this.clipProjected();
+    const props = [
+      this.hoverState ? this.hoverValue : this.value,
+      this.max,
+      this.step,
+      this.single,
+      this.ratingSymbols,
+    ];
 
     return html`
       <label part="label" ?hidden=${!this.label}>${this.label}</label>
@@ -371,7 +376,10 @@ export default class IgcRatingComponent extends SizableMixin(
           @mousemove=${this.hoverPreview ? this.handleMouseMove : nothing}
         >
           <slot @slotchange=${this.handleSlotChange}>
-            ${this.renderSymbols()}
+            ${guard(props, () => {
+              this.clipProjected();
+              return this.renderSymbols();
+            })}
           </slot>
         </div>
         <label part="value-label" ?hidden=${this.valueLabel.length === 0}>
