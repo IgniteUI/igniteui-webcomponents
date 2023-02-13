e3207732cbbe93ce6040baaab4db0e24fa9d8f13
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-02-13 13:43:35 +0200

    fix(input): update stories and styles for size attribute Closes #658

--- stories/story.ts
+++ stories/story.ts
@@ -2,6 +2,7 @@ import { TemplateResult } from 'lit';
 
 export type Direction = 'ltr' | 'rtl' | 'auto';
 export type Variant = 'light' | 'dark';
+export type Size = 'attribute' | 'small' | 'medium' | 'large';
 
 export interface Story<T, K> {
   (args: T, context: K): TemplateResult;
@@ -11,5 +12,10 @@ export interface Story<T, K> {
 }
 
 export interface Context {
-  globals: { theme: string; direction: Direction; variant: Variant };
+  globals: {
+    theme: string;
+    direction: Direction;
+    variant: Variant;
+    size: Size;
+  };
 }
