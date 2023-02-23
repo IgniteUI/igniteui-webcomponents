e29c9644cbbbc22de0a5ec362fefb8830b88ee79
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-02-23 11:36:34 +0200

    deps(storybook): update storybook to version 7

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,17 +1,14 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "framework": {
+    name: "@storybook/web-components-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
