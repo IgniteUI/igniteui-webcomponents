cce31a52b4e6b785ede8546217ad17c779e70be1
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2023-05-03 16:51:55 +0300

    build: Added storybook a11y addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
