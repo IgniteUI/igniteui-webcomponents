7c63a23249136997bfa63c1c7bcecd3585043b7c
desig9stein <mspopovv@gmail.com>
2023-07-31 13:51:51 +0300

    fix(*): fix dropdown in context of select and combo components.

--- src/components/combo/combo-header.ts
+++ src/components/combo/combo-header.ts
@@ -3,9 +3,11 @@ import { themes } from '../../theming/theming-decorator.js';
 import { styles } from './themes/light/header/combo-header.base.css.js';
 import { styles as bootstrap } from '../dropdown/themes/light/header/dropdown-header.bootstrap.css.js';
 import { styles as fluent } from '../dropdown/themes/light/header/dropdown-header.fluent.css.js';
+import { styles as indigo } from '../dropdown/themes/light/header/dropdown-header.material.css.js';
+import { styles as material } from '../dropdown/themes/light/header/dropdown-header.material.css.js';
 
 /* blazorSuppress */
-@themes({ bootstrap, fluent })
+@themes({ bootstrap, fluent, indigo, material })
 export default class IgcComboHeaderComponent extends LitElement {
   public static readonly tagName: string = 'igc-combo-header';
   public static override styles = styles;
