2162cd38e711b0ad9e87603ef47716222f076dc1
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-08-14 16:48:20 +0300

    build(styles): revert to seconds for styles elapsed time

--- scripts/build-styles.mjs
+++ scripts/build-styles.mjs
@@ -54,6 +54,6 @@ export async function buildThemes() {
   }
 
   report.success(
-    `Styles generated in ${Math.round((Date.now() - startTime))}ms`
+    `Styles generated in ${Math.round((Date.now() - startTime) / 1000)}s`
   );
 })();
