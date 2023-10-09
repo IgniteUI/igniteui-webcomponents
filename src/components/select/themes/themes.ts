5d7dd9e7930965f82155848484ada7bde6729fcb
desig9stein <mspopovv@gmail.com>
2023-10-09 15:40:54 +0300

    refactor(select): dark themes and file structure.

---
+++ src/components/select/themes/themes.ts
@@ -0,0 +1,52 @@
+import { css } from 'lit';
+import { Themes } from '../../../theming/types.js';
+
+// Shared Styles
+import { styles as material } from './shared/select.material.css.js';
+import { styles as bootstrap } from './shared/select.bootstrap.css.js';
+import { styles as fluent } from './shared/select.fluent.css.js';
+import { styles as indigo } from './shared/select.indigo.css.js';
+
+// Light Overrides
+import { styles as materialLight } from './light/select.material.css.js';
+import { styles as bootstrapLight } from './light/select.bootstrap.css.js';
+import { styles as fluentLight } from './light/select.fluent.css.js';
+import { styles as indigoLight } from './light/select.indigo.css.js';
+
+// Dark Overrides
+import { styles as materialDark } from './dark/select.material.css.js';
+import { styles as bootstrapDark } from './dark/select.bootstrap.css.js';
+import { styles as fluentDark } from './dark/select.fluent.css.js';
+import { styles as indigoDark } from './dark/select.indigo.css.js';
+
+const light = {
+  bootstrap: css`
+    ${bootstrap} ${bootstrapLight}
+  `,
+  material: css`
+    ${material} ${materialLight}
+  `,
+  fluent: css`
+    ${fluent} ${fluentLight}
+  `,
+  indigo: css`
+    ${indigo} ${indigoLight}
+  `,
+};
+
+const dark = {
+  bootstrap: css`
+    ${bootstrap} ${bootstrapDark}
+  `,
+  material: css`
+    ${material} ${materialDark}
+  `,
+  fluent: css`
+    ${fluent} ${fluentDark}
+  `,
+  indigo: css`
+    ${indigo} ${indigoDark}
+  `,
+};
+
+export const all: Themes = { light, dark };
