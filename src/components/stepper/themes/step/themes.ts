661e86109ac2163857a5496a3f5e506dc1099049
desig9stein <mspopovv@gmail.com>
2023-10-10 12:48:20 +0300

    refactor(stepper): dark themes and file structure.

---
+++ src/components/stepper/themes/step/themes.ts
@@ -0,0 +1,52 @@
+import { css } from 'lit';
+import { Themes } from '../../../../theming/types.js';
+
+// Shared Styles
+import { styles as material } from './shared/step.material.css.js';
+import { styles as bootstrap } from './shared/step.bootstrap.css.js';
+import { styles as fluent } from './shared/step.fluent.css.js';
+import { styles as indigo } from './shared/step.indigo.css.js';
+
+// Light Overrides
+import { styles as materialLight } from './light/step.material.css.js';
+import { styles as bootstrapLight } from './light/step.bootstrap.css.js';
+import { styles as fluentLight } from './light/step.fluent.css.js';
+import { styles as indigoLight } from './light/step.indigo.css.js';
+
+// Dark Overrides
+import { styles as materialDark } from './dark/step.material.css.js';
+import { styles as bootstrapDark } from './dark/step.bootstrap.css.js';
+import { styles as fluentDark } from './dark/step.fluent.css.js';
+import { styles as indigoDark } from './dark/step.indigo.css.js';
+
+const light = {
+  bootstrap: css`
+    ${bootstrap} ${bootstrapLight}
+  `,
+  material: css`
+    ${material} ${materialLight}
+  `,
+  fluent: css`
+    ${fluent} ${fluentLight}
+  `,
+  indigo: css`
+    ${indigo} ${indigoLight}
+  `,
+};
+
+const dark = {
+  bootstrap: css`
+    ${bootstrap} ${bootstrapDark}
+  `,
+  material: css`
+    ${material} ${materialDark}
+  `,
+  fluent: css`
+    ${fluent} ${fluentDark}
+  `,
+  indigo: css`
+    ${indigo} ${indigoDark}
+  `,
+};
+
+export const all: Themes = { light, dark };
