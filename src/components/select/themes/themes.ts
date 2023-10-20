4b30e283b2705c05658f8acc0af6994b087c6e0f
sivanova <sivanova@infragistics.com>
2023-10-20 10:07:15 +0300

    fix(list): wrong header colors

--- src/components/select/themes/themes.ts
+++ src/components/select/themes/themes.ts
@@ -5,8 +5,8 @@ import { styles as bootstrapDark } from './dark/select.bootstrap.css.js';
 import { styles as fluentDark } from './dark/select.fluent.css.js';
 import { styles as indigoDark } from './dark/select.indigo.css.js';
 import { styles as materialDark } from './dark/select.material.css.js';
-import { styles as bootstrapLight } from './light/select.bootstrap.css.js';
 // Light Overrides
+import { styles as bootstrapLight } from './light/select.bootstrap.css.js';
 import { styles as fluentLight } from './light/select.fluent.css.js';
 import { styles as indigoLight } from './light/select.indigo.css.js';
 import { styles as materialLight } from './light/select.material.css.js';
