71d58565436d36d10bd7ecffaf60598b3a156d7a
sivanova <sivanova@infragistics.com>
2024-01-16 11:36:09 +0200

    refactor(list/dropdown): include base schemas

--- src/components/dropdown/dropdown-header.ts
+++ src/components/dropdown/dropdown-header.ts
@@ -2,6 +2,7 @@ import { LitElement, html } from 'lit';
 
 import { styles } from './themes/dropdown-header.base.css.js';
 import { all } from './themes/header.js';
+import { styles as shared } from './themes/shared/header/dropdown-header.common.css.js';
 import { themes } from '../../theming/theming-decorator.js';
 import { registerComponent } from '../common/definitions/register.js';
 
@@ -15,7 +16,7 @@ import { registerComponent } from '../common/definitions/register.js';
 @themes(all)
 export default class IgcDropdownHeaderComponent extends LitElement {
   public static readonly tagName: string = 'igc-dropdown-header';
-  public static override styles = styles;
+  public static override styles = [styles, shared];
 
   public static register() {
     registerComponent(this);
