2eb0cf95ba734572c46e19edfd0dc39de0223730
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-03-11 17:23:12 +0200

    refactor: Migrate to globalThis references (#1096)

--- src/theming/config.spec.ts
+++ src/theming/config.spec.ts
@@ -2,8 +2,22 @@ import { expect, oneEvent } from '@open-wc/testing';
 
 import { configureTheme, getTheme } from './config.js';
 import { CHANGE_THEME_EVENT } from './theming-event.js';
+import { getAllCSSVariables } from './utils.js';
 
 describe('Theming Config', () => {
+  it('parses CSS variables from the document style sheets', async () => {
+    const sheet = document.createElement('style');
+    sheet.textContent = `:root { --igc-size: 1; --my-custom-size: 2rem }`;
+    document.head.append(sheet);
+
+    expect(getAllCSSVariables()).to.eql({
+      igcSize: '1',
+      myCustomSize: '2rem',
+    });
+
+    sheet.remove();
+  });
+
   it('should set the theme and raise event with the new theme', async () => {
     const theme = 'material';
     const themeVariant = 'light';
