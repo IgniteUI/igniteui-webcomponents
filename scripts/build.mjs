9856ee0d06039c7810f81ec63e0aec768a5a972a
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-04-24 16:39:32 +0300

    chore: Fix post merge issues

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -30,29 +30,55 @@ async function runTask(cmd, tag) {
   await runTask(exec.bind(null, 'npm run build:styles'), 'Styles');
 
   // https://github.com/microsoft/TypeScript/issues/14619
-  console.info('Building components...');
-  await exec(
-    'tsc -p scripts/tsconfig.prod.json && tsc -p scripts/tsconfig.dts.prod.json'
+  await runTask(
+    exec.bind(
+      null,
+      'tsc -p scripts/tsconfig.prod.json && tsc -p scripts/tsconfig.dts.prod.json'
+    ),
+    'Components'
   );
 
-  console.info('Generating theme files...');
-  await buildThemes();
+  await runTask(buildThemes, 'Themes');
 
-  console.info('Generating custom-elements.json file...');
-  await exec('npm run cem');
-  await copyFile('custom-elements.json', DEST_DIR('custom-elements.json'));
+  await runTask(
+    copyFile.bind(
+      null,
+      'custom-elements.json',
+      DEST_DIR('custom-elements.json')
+    ),
+    'custom-elements.json'
+  );
 
-  console.info('Generating package.json...');
-  await copyFile('scripts/_package.json', DEST_DIR('package.json'));
+  await runTask(
+    generateVsCodeCustomElementData.bind(null, customElements, {
+      outdir: 'dist',
+      cssFileName: 'igniteui-webcomponents.css-custom-data.json',
+      htmlFileName: 'igniteui-webcomponents.html-custom-data.json',
+    }),
+    'VSCode custom data'
+  );
 
-  console.info('Copying CHANGELOG.md...');
-  await copyFile('CHANGELOG.md', DEST_DIR('CHANGELOG.md'));
+  await runTask(
+    generateJetBrainsWebTypes.bind(null, customElements, { outdir: 'dist' }),
+    'JetBrains web types'
+  );
 
-  console.info('Copying LICENSE...');
-  await copyFile('LICENSE', DEST_DIR('LICENSE'));
+  await runTask(
+    copyFile.bind(null, 'scripts/_package.json', DEST_DIR('package.json')),
+    'package.json'
+  );
 
-  console.info('Copying README.md...');
-  await copyFile('README.md', DEST_DIR('README.md'));
+  await runTask(
+    copyFile.bind(null, 'CHANGELOG.md', DEST_DIR('CHANGELOG.md')),
+    'CHANGELOG'
+  );
 
-  console.log('Done! 🎉');
+  await runTask(copyFile.bind(null, 'LICENSE', DEST_DIR('LICENSE')), 'LICENSE');
+
+  await runTask(
+    copyFile.bind(null, 'README.md', DEST_DIR('README.md')),
+    'README'
+  );
+
+  report.success('Done! 🎉');
 })();
