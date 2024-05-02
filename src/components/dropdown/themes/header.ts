a8d690544d0437ec30a38c5a37d5eeafb4558959
sivanova <sivanova@infragistics.com>
2024-05-02 11:56:15 +0300

    feat(dropdown): update indigo styles

--- src/components/dropdown/themes/header.ts
+++ src/components/dropdown/themes/header.ts
@@ -1,16 +1,23 @@
 import { css } from 'lit';
 
 import type { Themes } from '../../../theming/types.js';
-// Shared Styles
 import { styles as fluent } from './shared/header/dropdown-header.fluent.css.js';
+// Shared Styles
+import { styles as indigo } from './shared/header/dropdown-header.indigo.css.js';
 
 const light = {
+  indigo: css`
+    ${indigo}
+  `,
   fluent: css`
     ${fluent}
   `,
 };
 
 const dark = {
+  indigo: css`
+    ${indigo}
+  `,
   fluent: css`
     ${fluent}
   `,
--- src/components/dropdown/themes/item.ts
+++ src/components/dropdown/themes/item.ts
@@ -1,16 +1,23 @@
 import { css } from 'lit';
 
 import type { Themes } from '../../../theming/types.js';
-// Shared Styles
 import { styles as fluent } from './shared/item/dropdown-item.fluent.css.js';
+// Shared Styles
+import { styles as indigo } from './shared/item/dropdown-item.indigo.css.js';
 
 const light = {
+  indigo: css`
+    ${indigo}
+  `,
   fluent: css`
     ${fluent}
   `,
 };
 
 const dark = {
+  indigo: css`
+    ${indigo}
+  `,
   fluent: css`
     ${fluent}
   `,
