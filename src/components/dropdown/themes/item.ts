ed46366d77cc75c7320399e9a73522298a5187ca
sivanova <sivanova@infragistics.com>
2024-05-02 11:59:53 +0300

    fix(dropdown): lint error

--- src/components/dropdown/themes/item.ts
+++ src/components/dropdown/themes/item.ts
@@ -1,8 +1,8 @@
 import { css } from 'lit';
 
 import type { Themes } from '../../../theming/types.js';
-import { styles as fluent } from './shared/item/dropdown-item.fluent.css.js';
 // Shared Styles
+import { styles as fluent } from './shared/item/dropdown-item.fluent.css.js';
 import { styles as indigo } from './shared/item/dropdown-item.indigo.css.js';
 
 const light = {
