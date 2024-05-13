a99ae4a9d2ca36f0127fe67b63535e12ef8d1e26
Simeon Simeonoff <sim.simeonoff@gmail.com>
2024-05-13 17:58:08 +0300

    refactor(navdrawer): fix header item imports

--- src/components/nav-drawer/themes/header-item.ts
+++ src/components/nav-drawer/themes/header-item.ts
@@ -1,10 +1,9 @@
 import { css } from 'lit';
-
 import type { Themes } from '../../../theming/types.js';
+
 // Shared Styles
 import { styles as bootstrap } from './shared/header-item/header-item.bootstrap.css.js';
 import { styles as indigo } from './shared/header-item/header-item.indigo.css.js';
-import { Themes } from '../../../theming/types.js';
 
 const light = {
   bootstrap: css`
