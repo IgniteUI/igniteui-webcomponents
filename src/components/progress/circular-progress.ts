0f087f9dcc1bf5c25e62261014eadd9116d5ab3b
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-06-06 14:27:57 +0300

    refactor(progressbars): Added CSS parts documentation (#1234)
    * Moved slotchange to a delegated handler
    * Some minor template cleanups

--- src/components/progress/circular-progress.ts
+++ src/components/progress/circular-progress.ts
@@ -20,19 +20,19 @@ import { all } from './themes/circular/themes.js';
  * @slot - The text area container.
  * @slot gradient - Customize the progress bar in order to use a color gradient instead of a solid color. Accepts `igc-circular-gradient` elements.
  *
- * @csspart svg
- * @csspart gradient_start
- * @csspart gradient_end
- * @csspart track
- * @csspart fill
- * @csspart label
- * @csspart value
- * @csspart indeterminate
- * @csspart primary
- * @csspart danger
- * @csspart warning
- * @csspart info
- * @csspart success
+ * @csspart svg - The igc-circular-progress SVG element.
+ * @csspart gradient_start - The igc-circular-progress linear-gradient start color.
+ * @csspart gradient_end - The igc-circular-progress linear-gradient end color.
+ * @csspart track - The igc-circular-progress ring track area.
+ * @csspart fill - The igc-circular-progress indicator area.
+ * @csspart label - The igc-circular-progress label.
+ * @csspart value - The igc-circular-progress label value.
+ * @csspart indeterminate - The igc-circular-progress indeterminate state.
+ * @csspart primary - The igc-circular-progress primary state.
+ * @csspart danger - The igc-circular-progress error state.
+ * @csspart warning - The igc-circular-progress warning state.
+ * @csspart info - The igc-circular-progress info state.
+ * @csspart success - The igc-circular-progress success state.
  */
 @themes(all)
 export default class IgcCircularProgressComponent extends IgcProgressBaseComponent {
@@ -92,7 +92,7 @@ export default class IgcCircularProgressComponent extends IgcProgressBaseCompone
 
     return html`
       <svg part=${partNameMap(parts)}>${this.renderSvg()}</svg>
-      <slot name="gradient" @slotchange=${this.slotChanged}></slot>
+      <slot name="gradient"></slot>
       ${this.renderDefaultSlot()}
     `;
   }
