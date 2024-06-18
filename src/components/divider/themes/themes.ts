57eb110ce6ea72f049a2550f0518858b99b8e430
sivanova <sivanova@infragistics.com>
2024-06-18 15:21:09 +0300

    fix(divider): remove base dark schema

--- src/components/divider/themes/themes.ts
+++ src/components/divider/themes/themes.ts
@@ -6,19 +6,18 @@ import { styles as bootstrapDark } from './dark/divider.bootstrap.css.js';
 import { styles as fluentDark } from './dark/divider.fluent.css.js';
 import { styles as indigoDark } from './dark/divider.indigo.css.js';
 import { styles as materialDark } from './dark/divider.material.css.js';
-import { styles as sharedDark } from './dark/divider.shared.css.js';
 // Light Overrides
 import { styles as bootstrapLight } from './light/divider.bootstrap.css.js';
 import { styles as fluentLight } from './light/divider.fluent.css.js';
 import { styles as indigoLight } from './light/divider.indigo.css.js';
 import { styles as materialLight } from './light/divider.material.css.js';
-import { styles as sharedLight } from './light/divider.shared.css.js';
+import { styles as shared } from './light/divider.shared.css.js';
 // Shared Styles
 import { styles as indigo } from './shared/divider.indigo.css.js';
 
 const light = {
   shared: css`
-    ${sharedLight}
+    ${shared}
   `,
   bootstrap: css`
     ${bootstrapLight}
@@ -36,7 +35,7 @@ const light = {
 
 const dark = {
   shared: css`
-    ${sharedDark}
+    ${shared}
   `,
   bootstrap: css`
     ${bootstrapDark}
