714b7d5126bd62bd21cacd9490ccc3343cbb889a
Simeon Simeonoff <sim.simeonoff@gmail.com>
2024-07-02 16:34:21 +0300

    refactor(stepper): split body and content animations

--- src/components/stepper/animations.ts
+++ src/components/stepper/animations.ts
@@ -1,61 +1,127 @@
 import { EaseOut } from '../../animations/easings.js';
-import { fadeIn, fadeOut } from '../../animations/presets/fade/index.js';
 import {
-  slideInHor,
-  slideOutHor,
-} from '../../animations/presets/slide/index.js';
-import { animation } from '../../animations/types.js';
+  type AnimationReferenceMetadata,
+  animation,
+} from '../../animations/types.js';
 
 const baseOptions: KeyframeAnimationOptions = {
-  duration: 350,
+  duration: 320,
   easing: EaseOut.Quad,
 };
 
-const growVerIn = (options = baseOptions) =>
+export type Animation = 'grow' | 'fade' | 'slide' | 'none';
+
+export type AnimationOptions = {
+  keyframe: KeyframeAnimationOptions;
+  step?: object;
+};
+
+const fadeIn = (options: AnimationOptions = { keyframe: baseOptions }) =>
+  animation([{ opacity: 0 }, { opacity: 1 }], options.keyframe);
+
+const fadeOut = (options: AnimationOptions = { keyframe: baseOptions }) =>
+  animation([{ opacity: 1 }, { opacity: 0 }], options.keyframe);
+
+const slideInHor = (
+  options: AnimationOptions = {
+    keyframe: baseOptions,
+  }
+) =>
   animation(
-    [
-      { opacity: 1, height: '32px' },
-      { opacity: 1, height: 'auto' },
-    ],
-    options
+    [{ transform: 'translateX(100%)' }, { transform: 'translateX(0)' }],
+    options.keyframe
   );
 
-const growVerOut = (options = baseOptions) =>
+const slideOutHor = (
+  options: AnimationOptions = {
+    keyframe: baseOptions,
+  }
+) =>
+  animation(
+    [{ transform: 'translateX(0)' }, { transform: 'translateX(-100%)' }],
+    options.keyframe
+  );
+
+const growVerIn = (
+  options: AnimationOptions = {
+    keyframe: baseOptions,
+    step: {},
+  }
+) =>
+  animation(
+    [
+      { opacity: 1, ...options.step },
+      { opacity: 1, height: 'auto' },
+    ],
+    options.keyframe
+  );
+
+const growVerOut = (
+  options: AnimationOptions = {
+    keyframe: baseOptions,
+    step: {},
+  }
+) =>
   animation(
     [
       { opacity: 1, height: 'auto' },
-      { opacity: 1, height: '32px' },
+      { opacity: 1, ...options.step },
     ],
-    options
+    options.keyframe
   );
 
 const noopAnimation = () => animation([], {});
-export type Animation = 'grow' | 'fade' | 'slide' | 'none';
-export const animations = new Map(
+
+const animationPair = (animations: {
+  in: (options: AnimationOptions) => AnimationReferenceMetadata;
+  out: (options: AnimationOptions) => AnimationReferenceMetadata;
+}) => {
+  return new Map(
+    Object.entries({
+      in: animations.in,
+      out: animations.out,
+    })
+  );
+};
+
+export const bodyAnimations = new Map(
   Object.entries({
-    grow: new Map(
-      Object.entries({
-        in: growVerIn,
-        out: growVerOut,
-      })
-    ),
-    fade: new Map(
-      Object.entries({
-        in: noopAnimation,
-        out: noopAnimation,
-      })
-    ),
-    slide: new Map(
-      Object.entries({
-        in: slideInHor,
-        out: slideOutHor,
-      })
-    ),
-    none: new Map(
-      Object.entries({
-        in: noopAnimation,
-        out: noopAnimation,
-      })
-    ),
+    grow: animationPair({
+      in: growVerIn,
+      out: growVerOut,
+    }),
+    fade: animationPair({
+      in: noopAnimation,
+      out: noopAnimation,
+    }),
+    slide: animationPair({
+      in: slideInHor,
+      out: slideOutHor,
+    }),
+    none: animationPair({
+      in: noopAnimation,
+      out: noopAnimation,
+    }),
+  })
+);
+
+export const contentAnimations = new Map(
+  Object.entries({
+    grow: animationPair({
+      in: fadeIn,
+      out: fadeOut,
+    }),
+    fade: animationPair({
+      in: fadeIn,
+      out: fadeOut,
+    }),
+    slide: animationPair({
+      in: fadeIn,
+      out: fadeOut,
+    }),
+    none: animationPair({
+      in: noopAnimation,
+      out: noopAnimation,
+    }),
   })
 );
