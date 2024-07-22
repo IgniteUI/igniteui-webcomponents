83c7383180cdcfa8601c751b561bc9b8bbfb05ff
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-07-22 14:17:24 +0300

    build: Refactored build:publish script (#1322)

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,91 +1,91 @@
 import { exec as _exec } from 'node:child_process';
-import { copyFile } from 'node:fs/promises';
+import { copyFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { promisify } from 'node:util';
-import { generateJetBrainsWebTypes } from 'custom-element-jet-brains-integration';
-import { generateVsCodeCustomElementData } from 'custom-element-vs-code-integration';
+import { getWebTypesData } from 'custom-element-jet-brains-integration';
+import {
+  getVsCodeCssCustomData,
+  getVsCodeHtmlCustomData,
+} from 'custom-element-vs-code-integration';
 import customElements from '../custom-elements.json' assert { type: 'json' };
 import { buildThemes } from './build-styles.mjs';
 import report from './report.mjs';
 
 const exec = promisify(_exec);
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const DEST_DIR = path.join.bind(null, path.resolve(__dirname, '../dist'));
+const DEST_DIR = path.join.bind(
+  null,
+  path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../dist')
+);
+const RELEASE_FILES = [
+  'custom-elements.json',
+  'CHANGELOG.md',
+  'LICENSE',
+  'README.md',
+];
 
-async function runTask(cmd, tag) {
-  report.info(`[${tag}] Building...`);
+async function runTask(tag, cmd) {
+  report.stdout.info(`[${tag}] Working...`);
+
   try {
     await cmd();
-    report.success(`[${tag}] Done`);
+    report.stdout.clearLine();
+    report.stdout.success(`[${tag}] Done\n`);
   } catch (e) {
     report.error(`[${tag}] Failed with: ${e}`);
   }
 }
 
 (async () => {
-  await exec('npm run clean');
-
-  await runTask(exec.bind(null, 'npm run build:styles'), 'Styles');
+  await runTask('Clean up', () => exec('npm run clean'));
+  await runTask('Styles', () => exec('npm run build:styles'));
 
   // https://github.com/microsoft/TypeScript/issues/14619
-  await runTask(
-    exec.bind(
-      null,
+  await runTask('Components', () =>
+    exec(
       'tsc -p scripts/tsconfig.prod.json && tsc -p scripts/tsconfig.dts.prod.json'
-    ),
-    'Components'
+    )
   );
 
-  await runTask(buildThemes, 'Themes');
+  await runTask('Themes', buildThemes);
 
-  await runTask(
-    copyFile.bind(
-      null,
-      'custom-elements.json',
-      DEST_DIR('custom-elements.json')
-    ),
-    'custom-elements.json'
+  await runTask('Copying release files', () =>
+    Promise.all([
+      copyFile('scripts/_package.json', DEST_DIR('package.json')),
+      ...RELEASE_FILES.map((file) => copyFile(file, DEST_DIR(file))),
+    ])
   );
 
-  await runTask(
-    generateVsCodeCustomElementData.bind(null, customElements, {
-      outdir: 'dist',
-      cssFileName: 'igniteui-webcomponents.css-data.json',
-      htmlFileName: 'igniteui-webcomponents.html-data.json',
-      hideMethodDocs: true,
-      hideCssPartsDocs: false,
-      hideLogs: true,
-    }),
-    'VSCode custom data'
-  );
-
-  await runTask(
-    generateJetBrainsWebTypes.bind(null, customElements, {
-      outdir: 'dist',
-      hideMethodDocs: true,
-      hideCssPartsDocs: false,
-      hideLogs: true,
-    }),
-    'JetBrains web types'
-  );
-
-  await runTask(
-    copyFile.bind(null, 'scripts/_package.json', DEST_DIR('package.json')),
-    'package.json'
-  );
-
-  await runTask(
-    copyFile.bind(null, 'CHANGELOG.md', DEST_DIR('CHANGELOG.md')),
-    'CHANGELOG'
-  );
-
-  await runTask(copyFile.bind(null, 'LICENSE', DEST_DIR('LICENSE')), 'LICENSE');
-
-  await runTask(
-    copyFile.bind(null, 'README.md', DEST_DIR('README.md')),
-    'README'
+  await runTask('VSCode custom data + Web types', () =>
+    Promise.all([
+      writeFile(
+        DEST_DIR('igniteui-webcomponents.css-data.json'),
+        getVsCodeCssCustomData(customElements, {
+          hideCssPartsDocs: false,
+          hideCssPropertiesDocs: false,
+          hideLogs: true,
+        }),
+        'utf8'
+      ),
+      writeFile(
+        DEST_DIR('igniteui-webcomponents.html-data.json'),
+        getVsCodeHtmlCustomData(customElements, {
+          hideMethodDocs: true,
+          hideLogs: true,
+        }),
+        'utf8'
+      ),
+      writeFile(
+        DEST_DIR('web-types.json'),
+        getWebTypesData(customElements, {
+          hideMethodDocs: true,
+          hideCssPartsDocs: false,
+          hideLogs: true,
+        }),
+        'utf8'
+      ),
+    ])
   );
 
   report.success('Done! 🎉');
