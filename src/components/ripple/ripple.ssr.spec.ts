9337c10ed260dd7c52d06b892aae99d6e916f296
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-09-09 16:40:36 +0300

    test(ripple): Added SSR fixtures

---
+++ src/components/ripple/ripple.ssr.spec.ts
@@ -0,0 +1,35 @@
+import {
+  cleanupFixtures,
+  csrFixture,
+  ssrHydratedFixture,
+  ssrNonHydratedFixture,
+} from '@lit-labs/testing/fixtures.js';
+
+import { html } from 'lit';
+import { isSsrRendered, isSsrStyled } from '../common/utils.spec.js';
+import type IgcRippleComponent from './ripple.js';
+
+afterEach(() => cleanupFixtures());
+
+const template = html`
+  <igc-button>
+    Click
+    <igc-ripple></igc-ripple>
+  </igc-button>
+`;
+
+for (const fixture of [csrFixture, ssrHydratedFixture, ssrNonHydratedFixture]) {
+  describe(`[${fixture.name}] - igc-rating`, () => {
+    it('renders as expected', async () => {
+      const element = await fixture<IgcRippleComponent>(template, {
+        modules: [
+          './ripple-auto-register.js',
+          '../button/button-auto-register.ts',
+        ],
+      });
+
+      isSsrRendered(element);
+      isSsrStyled(element);
+    });
+  });
+}
