7bf5a39af5d0206417c06058a0232ce2a493a88a
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-09-09 16:08:16 +0300

    test(icon): Added SSR fixtures

---
+++ src/components/icon/icon-auto-registry.ts
@@ -0,0 +1,5 @@
+// NOTE: Self-registering import for SSR fixtures.
+import Component from './icon.js';
+Component.register();
+
+export default Component;
---
+++ src/components/icon/icon.ssr.spec.ts
@@ -0,0 +1,27 @@
+import {
+  cleanupFixtures,
+  csrFixture,
+  ssrHydratedFixture,
+  ssrNonHydratedFixture,
+} from '@lit-labs/testing/fixtures.js';
+
+import { html } from 'lit';
+import { isSsrRendered, isSsrStyled } from '../common/utils.spec.js';
+import type IgcIconComponent from './icon.js';
+
+afterEach(() => cleanupFixtures());
+
+const template = html`<igc-icon name="icon" collection="icons"></igc-icon>`;
+
+for (const fixture of [csrFixture, ssrHydratedFixture, ssrNonHydratedFixture]) {
+  describe(`[${fixture.name}] - igc-icon`, () => {
+    it('renders as expected', async () => {
+      const element = await fixture<IgcIconComponent>(template, {
+        modules: ['./icon-auto-register.js'],
+      });
+
+      isSsrRendered(element);
+      isSsrStyled(element);
+    });
+  });
+}
