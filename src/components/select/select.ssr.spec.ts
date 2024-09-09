030146e0eb63bf850e1404eec2d93f5926ed2390
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-09-09 16:43:59 +0300

    test(select): Added SSR fixtures

--- src/components/select/select.ssr.spec.ts
+++ src/components/select/select.ssr.spec.ts
@@ -7,25 +7,25 @@ import {
 
 import { html } from 'lit';
 import { isSsrRendered, isSsrStyled } from '../common/utils.spec.js';
-import type IgcRippleComponent from './ripple.js';
+import type IgcSelectComponent from './select.js';
 
 afterEach(() => cleanupFixtures());
 
 const template = html`
-  <igc-button>
-    Click
-    <igc-ripple></igc-ripple>
-  </igc-button>
+  <igc-select>
+    <igc-select-header>Header</igc-select-header>
+    <igc-select-group>
+      <igc-select-item>1</igc-select-item>
+      <igc-select-item>2</igc-select-item>
+    </igc-select-group>
+  </igc-select>
 `;
 
 for (const fixture of [csrFixture, ssrHydratedFixture, ssrNonHydratedFixture]) {
-  describe(`[${fixture.name}] - igc-ripple`, () => {
+  describe(`[${fixture.name}] - igc-select`, () => {
     it('renders as expected', async () => {
-      const element = await fixture<IgcRippleComponent>(template, {
-        modules: [
-          './ripple-auto-register.js',
-          '../button/button-auto-register.ts',
-        ],
+      const element = await fixture<IgcSelectComponent>(template, {
+        modules: ['./select-auto-register.js'],
       });
 
       isSsrRendered(element);
