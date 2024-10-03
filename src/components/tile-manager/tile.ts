ff51d7c9c357a5a7504ac5a74ffce33c11b0410f
onlyexeption <ibarakov@infragistics.com>
2024-10-03 11:18:21 +0300

    feat(tile-manager): implement base resizing logic

--- src/components/tile-manager/tile.ts
+++ src/components/tile-manager/tile.ts
@@ -1,4 +1,4 @@
-import { LitElement, css, html } from 'lit';
+import { LitElement, html } from 'lit';
 
 import { registerComponent } from '../common/definitions/register.js';
 import { styles } from './themes/tile.base.css.js';
@@ -12,10 +12,52 @@ import { styles } from './themes/tile.base.css.js';
 export default class IgcTileComponent extends LitElement {
   public static readonly tagName = 'igc-tile';
   public static override styles = [styles];
+  protected activeResizer = '';
 
   /* blazorSuppress */
   public static register() {
     registerComponent(IgcTileComponent);
+  }
+
+  startResize(event: MouseEvent) {
+    event.preventDefault();
+
+    if (event.target instanceof Element) {
+      this.activeResizer = event.target.classList.contains('right')
+        ? 'right'
+        : event.target.classList.contains('bottom')
+          ? 'bottom'
+          : 'handle';
+    }
+
+    this.onResizing = this.onResizing.bind(this);
+    this.stopResize = this.stopResize.bind(this);
+
+    window.addEventListener('mousemove', this.onResizing);
+    window.addEventListener('mouseup', this.stopResize);
+  }
+
+  onResizing(event: MouseEvent) {
+    const startPos = this.getBoundingClientRect();
+
+    const newWidth = event.clientX - startPos.left;
+    const newHeight = event.clientY - startPos.top;
+
+    const colSpan = Math.max(1, Math.floor(newWidth / 30));
+    const rowSpan = Math.max(1, Math.floor(newHeight / 30));
+
+    if (this.activeResizer === 'right' || this.activeResizer === 'handle') {
+      this.style.gridColumn = `span ${colSpan}`;
+    }
+
+    if (this.activeResizer === 'bottom' || this.activeResizer === 'handle') {
+      this.style.gridRow = `span ${rowSpan}`;
+    }
+  }
+
+  stopResize() {
+    window.removeEventListener('mousemove', this.onResizing);
+    window.removeEventListener('mouseup', this.stopResize);
   }
 
   protected override render() {
@@ -23,6 +65,18 @@ export default class IgcTileComponent extends LitElement {
       <div part="base">
         <slot name="header"></slot>
         <slot></slot>
+        <div
+          class="resize-handle"
+          @mousedown=${this.startResize.bind(this)}
+        ></div>
+        <div
+          class="resizer right"
+          @mousedown=${this.startResize.bind(this)}
+        ></div>
+        <div
+          class="resizer bottom"
+          @mousedown=${this.startResize.bind(this)}
+        ></div>
       </div>
     `;
   }
