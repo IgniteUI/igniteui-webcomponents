ce78e2e857fd8ad39b57732d99540478041ad9d4
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2024-10-22 16:38:12 +0300

    refactor(utils): Removed `getOffset` helper function

--- src/components/common/util.ts
+++ src/components/common/util.ts
@@ -27,34 +27,6 @@ export function sameObject(a: object, b: object) {
   return JSON.stringify(a) === JSON.stringify(b);
 }
 
-/**
- *
- * Returns an element's offset relative to its parent. Similar to element.offsetTop and element.offsetLeft, except the
- * parent doesn't have to be positioned relative or absolute.
- *
- * Work around for the following issues in Chromium based browsers:
- *
- * https://bugs.chromium.org/p/chromium/issues/detail?id=1330819
- * https://bugs.chromium.org/p/chromium/issues/detail?id=1334556
- *
- */
-export function getOffset(element: HTMLElement, parent: HTMLElement) {
-  const { top, left, bottom, right } = element.getBoundingClientRect();
-  const {
-    top: pTop,
-    left: pLeft,
-    bottom: pBottom,
-    right: pRight,
-  } = parent.getBoundingClientRect();
-
-  return {
-    top: Math.round(top - pTop),
-    left: Math.round(left - pLeft),
-    right: Math.round(right - pRight),
-    bottom: Math.round(bottom - pBottom),
-  };
-}
-
 export function createCounter() {
   let i = 0;
   return () => {
