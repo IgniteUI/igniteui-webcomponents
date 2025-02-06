42b7bf5e2735de2397e4f170343ee1f04c604d13
onlyexeption <ibarakov@infragistics.com>
2025-02-06 15:50:59 +0200

    fix: await next frame in tests and fix tile part test

--- src/components/tile-manager/tile-resize.spec.ts
+++ src/components/tile-manager/tile-resize.spec.ts
@@ -1,4 +1,10 @@
-import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
+import {
+  elementUpdated,
+  expect,
+  fixture,
+  html,
+  nextFrame,
+} from '@open-wc/testing';
 import type Sinon from 'sinon';
 import { spy } from 'sinon';
 
@@ -148,6 +154,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.side);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(eventSpy).calledWith('igcResizeEnd');
       expect(DOM.ghostElement).to.be.null;
@@ -173,6 +180,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.side);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(getComputedStyle(firstTile).gridColumn).to.eql('auto / span 4');
       expect(getRows().length).to.eql(2);
@@ -189,6 +197,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.bottom);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(getComputedStyle(firstTile).gridRow).to.eql('auto / span 2');
 
@@ -204,6 +213,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.corner);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(getRows().length).to.eql(1);
       expect(getComputedStyle(firstTile).gridColumn).to.eql('auto / span 1');
@@ -236,6 +246,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.side);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(getComputedStyle(firstTile).gridColumn).to.eql('auto / span 10');
     });
@@ -274,6 +285,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.side);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(getComputedStyle(firstTile).gridColumn).to.eql('2 / span 4');
     });
@@ -299,6 +311,7 @@ describe('Tile resize', () => {
 
       simulateLostPointerCapture(DOM.adorners.bottom);
       await elementUpdated(DOM.resizeElement);
+      await nextFrame();
 
       expect(getComputedStyle(firstTile).gridRow).to.eql('2 / span 2');
     });
@@ -345,24 +358,14 @@ describe('Tile resize', () => {
       const DOM = getResizeContainerDOM(firstTile);
       const eventSpy = spy(DOM.resizeElement, 'emitEvent');
 
-      const tileRect = firstTile.getBoundingClientRect();
+      expect(firstTile.part.length).to.equal(0);
 
       simulatePointerDown(DOM.adorners.bottom);
       await elementUpdated(DOM.resizeElement);
 
       expect(eventSpy).calledWith('igcResizeStart');
-      expect(firstTile.part.length).to.equal(0);
-
-      simulatePointerMove(DOM.adorners.corner, {
-        clientX: tileRect.right * 2,
-        clientY: tileRect.bottom * 2,
-      });
-      await elementUpdated(DOM.resizeElement);
-
-      expect(eventSpy).calledWith('igcResize');
-      const resizeElementBaseWrapper =
-        DOM.resizeElement.querySelector('[part~="base"]')!;
-      expect(resizeElementBaseWrapper.part.contains('resizing')).to.be.true;
+      expect(firstTile.part.length).to.be.greaterThan(0);
+      expect(firstTile.part.contains('resizing')).to.be.true;
     });
   });
 });
