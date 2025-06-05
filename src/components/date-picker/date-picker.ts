09b07bf893d780c039ee1ba87de815ed608ab219
Maya <MKirova@infragistics.com>
2025-06-05 14:39:28 +0300

    fix: Add tags for blazor gen as indirect render and visual children support. (#1701)
    Added meta tags for calendar and date-picker
    
    ---------
    
    Co-authored-by: Radoslav Karaivanov <rkaraivanov@infragistics.com>

--- src/components/date-picker/date-picker.ts
+++ src/components/date-picker/date-picker.ts
@@ -69,6 +69,8 @@ export interface IgcDatePickerComponentEventMap {
 
 const formats = new Set(['short', 'medium', 'long', 'full']);
 
+/* blazorIndirectRender */
+/* blazorSupportsVisualChildren */
 /**
  * igc-date-picker is a feature rich component used for entering a date through manual text input or
  * choosing date values from a calendar dialog that pops up.
