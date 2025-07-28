dec124087f36a244e013d5c70aded21e27a3a55f
teodosiah <teodosiah@gmail.com>
2025-07-28 14:56:20 +0300

    fix(chat): fix file drag&drop

--- src/components/chat/chat-message.ts
+++ src/components/chat/chat-message.ts
@@ -78,7 +78,7 @@ export default class IgcChatMessageComponent extends LitElement {
    * - Renders attachments and custom templates if provided.
    */
   protected override render() {
-    const containerClass = `message-container ${this.message?.sender === this._chatState?.currentUserId ? 'sent' : ''}`;
+    const containerPart = `message-container ${this.message?.sender === this._chatState?.currentUserId ? 'sent' : ''}`;
     const sanitizedMessageText = this.sanitizeMessageText(
       this.message?.text.trim() || ''
     );
@@ -86,7 +86,7 @@ export default class IgcChatMessageComponent extends LitElement {
       this._chatState?.options?.markdownRenderer || renderMarkdown;
 
     return html`
-      <div part=${containerClass}>
+      <div part=${containerPart}>
         <div part="bubble">
           ${this._chatState?.options?.templates?.messageTemplate && this.message
             ? this._chatState.options.templates.messageTemplate(this.message)
