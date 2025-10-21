e035870cd6fc4aaca9b6b5be9d2584fc76d4779c
Svetoslav Krastev <striker18@mail.bg>
2025-10-21 13:13:59 +0300

    feat(localization): Integration of new i18nManager resource strings. (#1835)
    ---------
    
    Co-authored-by: Radoslav Karaivanov <rkaraivanov@infragistics.com>

---
+++ src/components/common/i18n/EN/calendar.resources.ts
@@ -0,0 +1,23 @@
+import { CalendarResourceStringsEN } from 'igniteui-i18n-core';
+import { convertToIgcResource } from '../utils.js';
+
+/* blazorSuppress */
+export interface IgcCalendarResourceStrings {
+  selectMonth: string;
+  selectYear: string;
+  selectDate: string;
+  selectRange: string;
+  selectedDate: string;
+  startDate: string;
+  endDate: string;
+  previousMonth: string;
+  nextMonth: string;
+  previousYear: string;
+  nextYear: string;
+  previousYears: string;
+  nextYears: string;
+  weekLabel: string;
+}
+
+export const IgcCalendarResourceStringEN: IgcCalendarResourceStrings =
+  convertToIgcResource(CalendarResourceStringsEN);
