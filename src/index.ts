e9f0112f07fd7bbb50f968438b37f26d981d1799
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2025-11-14 10:10:12 +0200

    feat: Added color picker component

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ export { default as IgcCarouselComponent } from './components/carousel/carousel.
 export { default as IgcCarouselIndicatorComponent } from './components/carousel/carousel-indicator.js';
 export { default as IgcCarouselSlideComponent } from './components/carousel/carousel-slide.js';
 export { default as IgcChatComponent } from './components/chat/chat.js';
+export { default as IgcColorPickerComponent } from './components/color-picker/color-picker.js';
 export { default as IgcCheckboxComponent } from './components/checkbox/checkbox.js';
 export { default as IgcCircularProgressComponent } from './components/progress/circular-progress.js';
 export { default as IgcCircularGradientComponent } from './components/progress/circular-gradient.js';
