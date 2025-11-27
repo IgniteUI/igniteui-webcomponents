2f670ddfc7bc222911dff91fef6d3898f67dc9e7
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2025-11-27 12:26:50 +0200

    fix(mask): Correct auto-fill handling with literals (#2006)
    When the browser auto-fills a masked input field, it may include literal characters
    as part of the filled value. The previous implementation did not correctly handle
    such cases, leading to incorrect parsing and display of the input value.

--- src/components/mask-input/mask-input-base.ts
+++ src/components/mask-input/mask-input-base.ts
@@ -119,8 +119,9 @@ export abstract class IgcMaskInputBaseComponent extends IgcInputBaseComponent {
 
       // Potential browser auto-fill behavior
       case undefined:
+      case '':
         return this._updateInput(
-          value.substring(start, this._inputSelection.end),
+          this._parser.parse(value.substring(start, this._inputSelection.end)),
           {
             start,
             end: this._inputSelection.end,
