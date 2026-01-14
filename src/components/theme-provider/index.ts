7e323b9b820ee0f3745ab09c64ba623d89d5d6ca
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2026-01-14 15:32:15 +0200

    delete: Removed theme provider component barrel index file

--- src/components/theme-provider/index.ts
+++
@@ -1,6 +0,0 @@
-import { defineComponents } from '../common/definitions/defineComponents.js';
-import IgcThemeProviderComponent from './theme-provider.js';
-
-defineComponents(IgcThemeProviderComponent);
-
-export { IgcThemeProviderComponent };
