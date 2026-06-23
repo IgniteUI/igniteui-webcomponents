87c9781a93f7302c865ef01e1cccfd5a152cc6b3
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2026-06-23 16:50:09 +0300

    refactor: Use updateComplete promise instead of queueMicrotask to get the commandFor element in button-base component

--- src/components/button/button-base.ts
+++ src/components/button/button-base.ts
@@ -190,15 +190,15 @@ export abstract class IgcButtonBaseComponent extends EventEmitterMixin<
   //#region Lifecycle
 
   protected override firstUpdated(): void {
-    if (this._commandfor) {
-      queueMicrotask(() => {
+    this.updateComplete.then(() => {
+      if (this._commandfor) {
         this._commandForElement = getElementByIdFromRoot(
           this,
-          this._commandfor!
+          this._commandfor
         );
         this.requestUpdate();
-      });
-    }
+      }
+    });
   }
 
   //#endregion
